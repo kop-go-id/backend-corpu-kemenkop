98ca16ca71b3fa323e62ff8234d1096a5f702c51
vinss-droid <kevinsipahutar220604@gmail.com>
2025-11-01 13:40:31 +0700

    rename column created_at & updated_at in users table

---
+++ database/migrations/2025_11_01_063903_rename_createdat_and_updatedat_in_users.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->renameColumn('createdAt', 'created_at');
+            $table->renameColumn('updatedAt', 'updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->renameColumn('created_at', 'createdAt');
+            $table->renameColumn('updated_at', 'updatedAt');
+        });
+    }
+};
